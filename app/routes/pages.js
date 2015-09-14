af8bd03af4530f3f6a77791a9104156a804abd90
crsr <cursaru.alexandru@gmail.com>
2015-09-15 00:47:45 +0300

    fixing pages.js file

--- app/routes/pages.js
+++ app/routes/pages.js
@@ -1,17 +1,17 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({  
+export default Ember.Route.extend({
     model() {
-        let pages = [
-            {
-                title: "Learn Ember",
-                complete: false,
-            },
-            {
-                title: "Solve World Hunger",
-                complete: false,
-            }
-        ];
-        return pages;
-    }
+    let pages = [
+        {
+            title: "Learn Ember",
+            complete: false,
+        },
+        {
+            title: "Solve World Hunger",
+            complete: false,
+        }
+    ];
+    return pages;
+}
 });
